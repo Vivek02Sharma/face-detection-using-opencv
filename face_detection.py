3b45071a2093bc80d49cfa42695ce407bc39d288
Vivek Sharma <112774647+Vivek02Sharma@users.noreply.github.com>
2024-01-07 19:31:46 +0530

    Add files via upload

---
+++ face_detection.py
@@ -0,0 +1,33 @@
+import cv2
+
+video = cv2.VideoCapture("src/baby_boy_video.webm")
+if not video.isOpened():
+    print("ERROR : Cannot open video")
+    exit()
+
+face_cascade = cv2.CascadeClassifier(
+    cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+while True:
+    ret, frame = video.read()
+    if not ret:
+        print("ERROR : Failed to capture frame.")
+
+    frame = cv2.flip(frame, 1)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    face_detect = face_cascade.detectMultiScale(
+        gray,
+        scaleFactor=1.15,
+        minNeighbors=6,
+    )
+
+    for (x, y, w, h) in face_detect:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
+
+    cv2.imshow("Video Windows", frame)
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+video.release()
+cv2.destroyAllWindows()
